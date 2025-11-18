73eb9cf09ec5df1377cdb98f6d0094246f087f46
Gnod <gnod@Gnods-MacBook-Pro.local>
2025-11-18 17:00:04 +0300

    add stack problem: Largest Rectangle In Histogram 84

--- NeetCode/Largest_Rectangle_In_Histogram_84.py
+++ NeetCode/Largest_Rectangle_In_Histogram_84.py
@@ -1,8 +1,21 @@
 class Solution:
     def largestRectangleArea(self, heights):
-        res = 0
-        return res
+        maxArea = 0
+        stack = [] # pair: (index, height)
 
-heights = [2,1,5,6,2,3]
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                maxArea = max(maxArea, height * (i - index))
+                start = index
+            stack.append([start, h])
+
+        for index, height in stack:
+            maxArea = max(maxArea, height * (len(heights) - index))
+
+        return maxArea
+
+heights = [7,1,7,2,2,4]
 sol = Solution()
 print(sol.largestRectangleArea(heights))
